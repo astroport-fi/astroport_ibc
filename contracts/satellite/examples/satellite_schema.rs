5ee0a935eeeebb4aaeea2bd5f0d5e671c4b411b2
Timofey <epanchee@gmail.com>
2022-10-07 15:24:58 +0300

    Feat(schema): add schema generators (#6)

---
+++ contracts/satellite/examples/satellite_schema.rs
@@ -0,0 +1,10 @@
+use astro_satellite_package::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cosmwasm_schema::write_api;
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        query: QueryMsg,
+        execute: ExecuteMsg,
+    }
+}
