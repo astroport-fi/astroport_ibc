fdbce9ff317216ef939560e81be7811bbf4fcfa0
P-Yevhenii <86720500+P-Yevhenii@users.noreply.github.com>
2022-10-12 14:12:25 +0200

    feat(scripts): added deploy scripts (release/terra2) (#9)

---
+++ scripts/build_release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+projectPath=$(cd "$(dirname "${0}")" && cd ../ && pwd)
+echo $projectPath
+
+docker run --rm -v "$projectPath":/code \
+  --mount type=volume,source="$(basename "$projectPath")_cache",target=/code/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/workspace-optimizer:0.12.6
